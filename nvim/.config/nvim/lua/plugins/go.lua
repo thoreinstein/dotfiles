5b327f550a0489e5760003bd86fda4b745e75ce2
Jim Myers <myersjim223@gmail.com>
2025-07-22 08:05:53 -0400

    Switch go plugins

--- nvim/.config/nvim/lua/plugins/go.lua
+++ nvim/.config/nvim/lua/plugins/go.lua
@@ -1,9 +1,37 @@
 return {
-  "fatih/vim-go",
-  init = function()
-    -- Disable vim-go auto formatting since we use conform.nvim
-    vim.g.go_fmt_autosave = 0
-    vim.g.go_imports_autosave = 0
-    vim.g.go_mod_fmt_autosave = 0
+  "ray-x/go.nvim",
+  dependencies = {
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("go").setup {
+      -- Disable auto format since we use conform.nvim
+      lsp_cfg = {
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+          },
+        },
+      },
+      lsp_keymaps = false, -- Use global LSP keymaps
+      fmt_on_save = false, -- We use conform.nvim
+      lsp_inlay_hints = {
+        enable = true,
+      },
+      trouble = true, -- Enable trouble.nvim integration
+      dap_debug = true, -- Enable dap debug support
+      test_runner = "ginkgo", -- go, richgo, dlv, ginkgo
+      run_in_floaterm = false, -- Use floaterm for running commands
+      icons = { breakpoint = "🔴", currentpos = "🔶" },
+    }
   end,
+  event = { "CmdlineEnter" },
+  ft = { "go", "gomod" },
+  build = ':lua require("go.install").update_all_sync()',
 }
