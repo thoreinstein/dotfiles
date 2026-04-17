b8ea721292b32030d2ca8d906facb2d72ad9fc76
Jim Myers <thoreinstein8@gmail.com>
2026-04-17 10:09:26 -0400

    Add mcp server configs

--- hosts/mac-1QFL40HG.nix
+++ hosts/mac-1QFL40HG.nix
@@ -1,12 +1,69 @@
 { lib, ... }:
 {
   programs = {
-    gemini-cli.settings = lib.importJSON ./mac-1QFL40HG/gemini-cli/settings.json;
+    codex = {
+      enable = true;
+      enableMcpIntegration = true;
+      settings = lib.importTOML ./mac-1QFL40HG/codex/config.toml;
+    };
 
-    codex.settings = lib.importTOML ./mac-1QFL40HG/codex/config.toml;
+    gemini-cli = {
+      enable = true;
+      enableMcpIntegration = true;
+      settings = lib.importJSON ./mac-1QFL40HG/gemini-cli/settings.json;
+    };
+
+    mcp = {
+      enable = true;
+      servers = {
+        context7 = {
+          url = "https://mcp.context7.com/mcp";
+        };
+        exa = {
+          url = "https://mcp.exa.ai/mcp?tools=web_search_exa";
+        };
+        github = {
+          url = "https://api.githubcopilot.com/mcp/";
+          headers = {
+            "Authorization" = "Bearer \${GITHUB_PERSONAL_ACCESS_TOKEN}";
+          };
+        };
+        grep_app = {
+          url = "https://mcp.grep.app";
+        };
+        playwright = {
+          command = "docker";
+          args = [
+            "run"
+            "-i"
+            "--rm"
+            "mcp/playwright"
+          ];
+        };
+        sequential_thinking = {
+          command = "docker";
+          args = [
+            "run"
+            "-i"
+            "--rm"
+            "mcp/sequentialthinking"
+          ];
+        };
+        time = {
+          command = "docker";
+          args = [
+            "run"
+            "-i"
+            "--rm"
+            "mcp/time"
+          ];
+        };
+      };
+    };
 
     opencode = {
       enable = true;
+      enableMcpIntegration = true;
       agents = ./mac-1QFL40HG/opencode/agents;
       commands = ./mac-1QFL40HG/opencode/commands;
       settings = {
@@ -37,6 +94,16 @@
           "CONTRIBUTING.md"
           "docs/*.md"
         ];
+        mcp = {
+          github = {
+            enabled = true;
+            type = "remote";
+            url = "https://api.githubcopilot.com/mcp/";
+            headers = {
+              "Authorization" = "Bearer {env:GITHUB_PERSONAL_ACCESS_TOKEN}";
+            };
+          };
+        };
         permission = {
           bash = "allow";
           codesearch = "allow";
